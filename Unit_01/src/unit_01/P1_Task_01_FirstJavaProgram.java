24d2abbf0ca52161250537c8efad770434c2896e
saurabh09ved <94731107+saurabh09ved@users.noreply.github.com>
2022-03-26 09:47:34 +0530

    constants in java

--- Unit_01/src/unit_01/P1_Task_01_FirstJavaProgram.java
+++ Unit_01/src/unit_01/P1_Task_01_FirstJavaProgram.java
@@ -3,5 +3,6 @@ package unit_01;
 public class P1_Task_01_FirstJavaProgram {
 	public static void main(String [] args) {
 		System.out.println("SAURABH VEDWAL");
-	}
+		System.out.println("its my first program");
+	} 
 }
