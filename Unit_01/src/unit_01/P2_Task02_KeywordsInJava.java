d69971b5fa701599f74a38661de4e2a7a48c0d63
saurabh09ved <94731107+saurabh09ved@users.noreply.github.com>
2022-03-26 10:17:51 +0530

    commits

--- Unit_01/src/unit_01/P2_Task02_KeywordsInJava.java
+++ Unit_01/src/unit_01/P2_Task02_KeywordsInJava.java
@@ -1,9 +1,18 @@
 package unit_01;
 //50 keywords in java
 //48 of use and 2 of future use
-//
 public class P2_Task02_KeywordsInJava {
 	public static void main (String[] args) {
-		System.out.println("")
+		//method
+		void int() {			//error invalid method name
+			System.out.println("WE ARE INSIDE FUNCTION");
+		}
+		//variable
+		int double =90		//invalid variable declaration
+		//object
+		ABC int =new ABC();
 	}
+}
+class int{ 				//invalid class name as keyword cannot be class name
+	System.out.println("INSIDE CLASS ")
 }
