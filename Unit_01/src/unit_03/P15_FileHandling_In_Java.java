2f4c4d30fd93d2eac209578b74adfe297b560be7
saurabh09ved <94731107+saurabh09ved@users.noreply.github.com>
2022-05-29 13:10:32 +0530

    final_unit_03

--- Unit_01/src/unit_03/P15_FileHandling_In_Java.java
+++ Unit_01/src/unit_03/P15_FileHandling_In_Java.java
@@ -1,5 +1,114 @@
 package unit_03;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+/*
+ * - Byte and Character Streams
+ * - Reading and writing from console and file
+ * */
 
 public class P15_FileHandling_In_Java {
+
+	public static void main(String[] args) throws IOException {
+
+		File_operations obj = new File_operations();
+		obj.takeInputFromAFile();
+		obj.saveOutputIntoAFile();
+		obj.saveLogOfProgram();
+
+	}
+
+}
+
+class File_operations {
+	
+	static int a = 10;
+
+	void takeInputFromAFile() throws IOException {
+
+		String path = "C:\\Users\\vedwa\\git\\java_with_saurabh\\Unit_01\\src\\unit_03\\Input.txt";
+		File file = new File(path);
+
+		BufferedReader br = new BufferedReader(new FileReader(file));
+
+//		String st;
+//		while ((st = br.readLine()) != null) {
+//			System.out.println(st);
+//		}
+
+		// list that holds strings of a file
+		List<String> listOfStrings = new ArrayList<String>();
+
+		// read entire line as string
+		String line = br.readLine();
+
+		while (line != null) {
+			listOfStrings.add(line);
+			line = br.readLine();
+		}
+
+		for (String str : listOfStrings) {
+			System.out.println(str);
+		}
+		System.out.println("\n");
+		br.close();
+	}
+
+	void saveOutputIntoAFile() throws IOException {
+
+		FileWriter myObj = null;
+
+		try {
+			myObj = new FileWriter("C:\\Users\\vedwa\\git\\java_with_saurabh\\Unit_01\\src\\unit_03\\output.txt");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		BufferedWriter f_writer = new BufferedWriter(myObj);
+
+		String text = "Hey this would store in Output file";
+
+		f_writer.write(text);
+		f_writer.close();
+
+	}
+
+	void saveLogOfProgram() throws IOException {
+		
+		a++;
+		
+		//Find current time and Date
+		LocalTime ltime = java.time.LocalTime.now();
+		LocalDate ldate = java.time.LocalDate.now();
+		
+		FileWriter myObj = null;
+
+		try {
+			myObj = new FileWriter("C:\\Users\\vedwa\\git\\java_with_saurabh\\Unit_01\\src\\unit_03\\log.txt",true);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		BufferedWriter f_writer = new BufferedWriter(myObj);
+
+		String text = "The Program executed at: " + ltime.toString() + " " + ldate.toString() + "\n";
+
+		try {
+			f_writer.write("Value of a: " + a + ", ");
+			f_writer.write(text);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		f_writer.close();
+
+	}
 
 }
--- Unit_01/src/unit_03/P16_MultiThreading_In_Java.java
+++ Unit_01/src/unit_03/P16_MultiThreading_In_Java.java
@@ -1,5 +1,117 @@
 package unit_03;
+import java.util.Scanner;
+/*
+ * program counter:-	program counter holds the address of next instruction to be executed by one processing unit.	
+ 
+ * multiprocessing:-	
+  		-	parallel processing
+  		-	sequential processing 
+  		-	concurrent processing
+ 
+ * Multitasking:-	
+  		-	parallel multitasking
+  		-	sequential multitasking 
+  		-	concurrent multitasking
+  		
 
+ * multithreading task:-
+   		-	it is the users job to find the independent task of that process.
+   		-	your findings are T1,T2,T3,T4 task of process P1 out of 8 total task.
+   		-	context switching between these tasks and the worst time is not decided by the user insted jvm will decide this .
+  		-	multithreading is applied when concurrent multitasking occurs.
+  		-	it is the users job to put these independent codes into there respected threads(T1,T2,T3,T4).
+ 		-	user have to start these thread by T1.start(),T2.start(),T3.start(),T4.start(). 
+*/
 public class P16_MultiThreading_In_Java {
 
+	public static void main(String[] args) {
+		
+		
+//		C1 obj1=new C1();
+//		C2 obj2=new C2();
+//		
+//		obj1.show1();
+//		obj2.show2();
+		
+		Thread obj3=new Thread(new T1(),"T1");
+		Thread obj4=new Thread(new T2(),"T2");
+		obj3.start();
+		obj4.start();
+		
+		Thread obj5=new Thread(new T3(),"T3");
+		Thread obj6=new Thread(new T4(),"T4");
+		obj5.start();
+		obj6.start();
+	}
+
+}
+
+
+class C1
+{
+	void show1()
+	{
+		for(int i=0;i<10;i++)
+		{
+			System.out.println("hey_1");
+		}
+	}
+}
+class C2
+{
+	void show2()
+	{
+		for(int i=0;i<10;i++)
+		{
+			System.out.println("hey_2");
+		}
+	}
+}
+
+class T1 implements Runnable{
+
+	
+	public void run() {
+		int a;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		for(int i=0;i<10;i++)
+		{
+			System.out.println("hey_1");
+		}
+	}
+	
+}
+class T2 implements Runnable{
+
+	public void run() {
+		for(int i=0;i<10;i++)
+		{
+			System.out.println("hey_2");
+		}
+	}
+	
+}
+
+
+class T3 extends Thread{
+
+	public void run() {
+		try {
+			Thread.sleep(10000);
+		} 
+		catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("T3 is done");
+	}
+	
+}
+class T4 implements Runnable{
+
+	public void run() {
+		for(int i=0;i<10;i++)
+			System.out.println("hey_3");
+	}
+	
 }
